de99274f03247bcebc101fa8b9a1d31c1d84a95f
matias1218 <matias.paredes@usach.cl>
2019-05-12 21:26:58 -0400

    se arreglo detalle trasero de vista gestionProfesores

--- src/views/GestionarProfesores.vue
+++ src/views/GestionarProfesores.vue
@@ -1,117 +1,118 @@
 <template>
-  <div id="gestionprofesores">
-   <v-app>
-    <v-card id="card1">
-     <v-card-title>
-         Gestionar Profesores    
-      </v-card-title>
-      <v-form v-model="valid">
-      <v-container>
-        <v-layout>
-          <v-flex
-            xs12
-            md4
-          >
-            <v-text-field
-              v-model="firstname"
-              :rules="nameRules"
-              label="Nombre"
-              required
-            ></v-text-field>
-          </v-flex>
-  
-          <v-flex
-            xs12
-            md4
-          >
-            <v-text-field
-              v-model="lastname"
-              :rules="nameRules"
-              label="Apellidos"
-              required
-            ></v-text-field>
-          </v-flex>
-  
-          <v-flex
-            xs12
-            md4
-          >
-            <v-text-field
-              v-model="topic"
-              :rules="topicRules"
-              label="Tópico"
-              required
-            ></v-text-field>
+  <v-container>
+    <v-layout row wrap justify-center>
 
-          </v-flex>
-          <v-flex
-            xs12
-            md4
-          >
-            <v-autocomplete
-            ref="carga"
-            v-model="carga"
-            :rules="[() => !!carga || 'Carga horaria requerida']"
-            :items="cargas"
-            label="Jornada"
-            required
-          ></v-autocomplete>
-            
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <v-btn round color="info">Agregar</v-btn>
-    </v-form>
-    
-     <v-card>
+      <v-card id="card1">
       <v-card-title>
-        Listado Profesores
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="search"
-          append-icon="search"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :search="search"
-        
-      >
-        <template v-slot:items="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.ap }}</td>
-          <td class="text-xs-right">{{ props.item.carga }}</td>
-          <td class="text-xs-right">{{ props.item.espe }}</td>
-          <td >
-            <v-icon
-              small
-              class="mr-2"
-              @click="editItem(props.item)"
+          Gestionar Profesores    
+        </v-card-title>
+        <v-form v-model="valid">
+        <v-container>
+          <v-layout>
+            <v-flex
+              xs12
+              md4
             >
-              edit
-            </v-icon>
-            <v-icon
-              small
-              @click="deleteItem(props.item)"
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                label="Nombre"
+                required
+              ></v-text-field>
+            </v-flex>
+    
+            <v-flex
+              xs12
+              md4
             >
-              delete
-            </v-icon>
-          </td>
-        </template>
-        <template v-slot:no-results>
-          <v-alert :value="true" color="error" icon="warning">
-             "{{ search }}" no se ha encontrado.
-          </v-alert>
-        </template>
-      </v-data-table>
-    </v-card>
-    </v-card>
-   </v-app>
- </div>
+              <v-text-field
+                v-model="lastname"
+                :rules="nameRules"
+                label="Apellidos"
+                required
+              ></v-text-field>
+            </v-flex>
+    
+            <v-flex
+              xs12
+              md4
+            >
+              <v-text-field
+                v-model="topic"
+                :rules="topicRules"
+                label="Tópico"
+                required
+              ></v-text-field>
+
+            </v-flex>
+            <v-flex
+              xs12
+              md4
+            >
+              <v-autocomplete
+              ref="carga"
+              v-model="carga"
+              :rules="[() => !!carga || 'Carga horaria requerida']"
+              :items="cargas"
+              label="Jornada"
+              required
+            ></v-autocomplete>
+              
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-btn round color="info">Agregar</v-btn>
+      </v-form>
+      
+      <v-card>
+        <v-card-title>
+          Listado Profesores
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :search="search"
+          
+        >
+          <template v-slot:items="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-right">{{ props.item.ap }}</td>
+            <td class="text-xs-right">{{ props.item.carga }}</td>
+            <td class="text-xs-right">{{ props.item.espe }}</td>
+            <td >
+              <v-icon
+                small
+                class="mr-2"
+                @click="editItem(props.item)"
+              >
+                edit
+              </v-icon>
+              <v-icon
+                small
+                @click="deleteItem(props.item)"
+              >
+                delete
+              </v-icon>
+            </td>
+          </template>
+          <template v-slot:no-results>
+            <v-alert :value="true" color="error" icon="warning">
+              "{{ search }}" no se ha encontrado.
+            </v-alert>
+          </template>
+        </v-data-table>
+      </v-card>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
